3cecfaff419c51bcba442e3100c6e6935929e2a6
Kyle <obrienk@webbhost.net>
2020-01-22 15:03:31 -0600

    creating new pages with gatsby-node.js, only gql query written

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,26 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
-
-// You can delete this file if you're not using it
+const path = require(`path`)
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+  return graphql(`
+    {
+      allBigCommerceProducts {
+        edges {
+          node {
+            name
+            description
+            price
+            images{
+                description
+              url_standard
+          }
+          custom_url{
+              url
+          }
+          }
+        }
+      }
+    }
+  `).then(result => {
+    console.log(JSON.stringify(result, null, 4))
+  })
+}
--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,6 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
-require('./animate.css')
 
 const Header = ({ siteTitle }) => (
   <header
